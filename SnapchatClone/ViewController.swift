57cf9d346ae6f84210859f11b13ba5d0bbcc3181
Yunus MAKAN <makanyunus@gmail.com>
2020-07-06 14:55:24 +0300

    Initial Commit

---
+++ SnapchatClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by MAKAN on 6.07.2020.
+//  Copyright © 2020 YUNUS MAKAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
