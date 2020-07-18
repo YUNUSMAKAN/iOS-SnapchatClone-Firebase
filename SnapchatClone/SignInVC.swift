8614453fa4e2c6044e24c72183b82cfc3cb3cbb9
Yunus MAKAN <makanyunus@gmail.com>
2020-07-18 15:46:42 +0300

    Initial commit!

---
+++ SnapchatClone/SignInVC.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by MAKAN on 6.07.2020.
+//  Copyright © 2020 YUNUS MAKAN. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    @IBAction func signInClicked(_ sender: Any) {
+    }
+    @IBAction func signUpClicked(_ sender: Any) {
+    }
+    
+}
+
